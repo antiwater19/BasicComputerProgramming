4ae6459dc9e09ac98127fcdc76ae6f035f89f252
antiwater19 <minsusu01@gmail.com>
2025-01-13 09:57:37 +0900

    2025.01.13_cold

--- 실습/실습.c
+++ 실습/실습.c
@@ -966,7 +966,7 @@ int ex0110_7(void) // 순차 정렬
 	return 0;
 }
 
-int main(void)
+int ex0110_8(void)
 {
 	int key, i;
 	int list[SIZE2] = { 1,2,3,4,5,6,7,8,9 };
@@ -980,5 +980,62 @@ int main(void)
 		}
 	}
 	printf("탐색종료\n");
+	return 0;
+}
+
+int ex0113_1(void)
+{
+	int scores[10] = { 0 };
+	int tot = 0;
+	float	average;
+	
+	for (int i = 1; i <= 10; i++) {
+		printf("%d 학생 점수를 입력하세요. ", i);
+		scanf("%d", &scores[i - 1]);
+		tot += scores[i - 1];
+	}
+	average = tot / 10;
+	printf("학생 총점 \t %d, 평균은 %.2f입니다.", tot, average);
+	return 0;
+}
+
+// 이진탐색
+#define SIZE4 16
+int binary_search(int list[], int n, int key)
+{
+	int low, high, middle;
+	low = 0;
+	high = n - 1;
+	while (low <= high) {
+		printf("[%d %d]\n", low, high);
+		middle = (low + high) / 2;
+		if (key == list[middle]) {
+			return middle;
+		}
+		else if (key > list[middle]) {
+			low = middle + 1;
+		}
+		else
+			high = middle - 1;
+	}
+	return -1;
+}
+
+int main(void)
+{
+	int key;
+	int grade[SIZE4] =
+	{ 2,6,11,13,18,20,22,27,29,30,34,38,41,42,45,47 };
+
+	printf("탐색할 값을 입력하시오.: ");
+	scanf("%d", &key);
+	int goal = binary_search(grade, SIZE4, key);
+
+	if (goal < 0) {
+		printf("탐색 결과 없음\n");
+	}
+	else
+		printf("탐색 결과 : grade[%d] = %d\n", goal, grade[goal]);
+
 	return 0;
 }
